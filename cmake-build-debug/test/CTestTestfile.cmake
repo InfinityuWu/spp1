293da09b7467d543e14492197a30ef1f82c2a281
Julian Meister <julianknapp8@gmail.com>
2023-11-20 21:05:27 +0100

    init

---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Julian Meister/.vscode/lab1/test
+# Build directory: C:/Users/Julian Meister/.vscode/lab1/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(SerialTests "C:/Users/Julian Meister/.vscode/lab1/cmake-build-debug/bin/lab_test.exe")
+set_tests_properties(SerialTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Julian Meister/.vscode/lab1/test/CMakeLists.txt;66;add_test;C:/Users/Julian Meister/.vscode/lab1/test/CMakeLists.txt;0;")
+subdirs("../_deps/googletest-build")
---
+++ cmake-build-debug/test_get_path.cpp
@@ -0,0 +1,2 @@
+#include <filesystem>
+std::filesystem::path get_path() { return "C:/Users/Julian Meister/.vscode/lab1"; }
